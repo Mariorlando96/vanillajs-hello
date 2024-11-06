bf2bd56ddcccd8c273691895a1685590dd3b0735
Mario Orol <141705874+Mariorlando96@users.noreply.github.com>
2024-11-06 13:49:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [
+  "The junior developer",
+  "My teammate",
+  "The client",
+  "The project manager"
+];
+let action = ["deleted", "broke", "overwrote", "forgot to save"];
+let what = [
+  "the HTML file",
+  "the CSS stylesheet",
+  "the JavaScript function",
+  "the entire project folder"
+];
+let when = [
+  "during the code review",
+  "right before deployment",
+  "when I pushed to production",
+  "while debugging",
+  "during the live demo"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  document.getElementById("excuse").innerText = excuse;
 };
